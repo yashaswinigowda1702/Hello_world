9ad32b1dcb0072df2ca6f138d1865a3bd5bbc494
yashaswinigowda1702 <yashaswinigowda171@gmail.com>
2024-05-22 09:38:52 +0530

    Update applIcatIon.py

--- applIcatIon.py
+++ applIcatIon.py
@@ -1,1 +1,8 @@
-print('Hello_world")
+print("Hello_world") 
+
+import streamlit as st 
+st.text("Streamlit app hello world")
+st.header("App header")
+st.header(My first app")
+st.textprint('Hello_world")
+
